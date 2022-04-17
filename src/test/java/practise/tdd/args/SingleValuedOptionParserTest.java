0584fc03c21a137af86b29ddece7049716908484
acmerfight <acmerfight@gmail.com>
2022-04-17 16:42:31 +0800

    add test: should_not_accept_insufficient_argument_for_single_valued_option

--- src/test/java/practise/tdd/args/SingleValuedOptionParserTest.java
+++ src/test/java/practise/tdd/args/SingleValuedOptionParserTest.java
@@ -1,6 +1,8 @@
 package practise.tdd.args;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -17,10 +19,12 @@ class SingleValuedOptionParserTest {
         assertEquals("p", e.getOption());
     }
 
-    @Test
-    void should_not_accept_insufficient_argument_for_single_valued_option() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-p -l", "-p"})
+    void should_not_accept_insufficient_argument_for_single_valued_option(String input) {
+        List<String> arguments = asList(input.split(" "));
         InsufficientArgumentsException e = assertThrows(InsufficientArgumentsException.class,
-                () -> new SingleValuedOptionParser<>(Integer::parseInt).parse(List.of("-p", "-l"), option("p")));
+                () -> new SingleValuedOptionParser<>(Integer::parseInt).parse(arguments, option("p")));
         assertEquals("p", e.getOption());
 
     }
