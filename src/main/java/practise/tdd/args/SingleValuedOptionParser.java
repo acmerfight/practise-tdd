8a39df1d6cb7bef7744b90b4bfce19322b4ec5ab
acmerfight <acmerfight@gmail.com>
2022-04-20 09:03:00 +0800

    refactor

--- src/main/java/practise/tdd/args/SingleValuedOptionParser.java
+++ src/main/java/practise/tdd/args/SingleValuedOptionParser.java
@@ -8,6 +8,7 @@ package practise.tdd.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 /**
  * @author yaogangqiang
@@ -28,14 +29,25 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
         if (index == -1) {
             return defaultValue;
         }
-        if (index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-")) {
+
+        List<String> values = getValues(arguments, index);
+
+        if (values.size() < 1) {
             throw new InsufficientArgumentsException(option.value());
         }
-        if (index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-")) {
+        if (values.size() > 1) {
             throw new TooManyArgumentsException(option.value());
         }
         String value = arguments.get(index + 1);
         return valueParser.apply(value);
+    }
+
+    private List<String> getValues(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst().orElse(arguments.size());
+
+        return arguments.subList(index + 1, followingFlag);
     }
 
 }
